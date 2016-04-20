536c1d018cf3ebb7c274c1b1b49af434587a8c8e
LongHairedHacker <sebastian@sebastians-site.de>
2016-04-20 21:54:29 +0200

    Started playing around

--- seidenstrasse.py
+++ seidenstrasse.py
@@ -5,19 +5,22 @@ from verdandi.modules.page import Page
 from verdandi.modules.commonassets import CommonAssets
 
 
-class MainPage(Page):
+class LandingPage(Page):
 	title = "Seidenstrasse :: OpenSource Rohrpost"
 	menu_title = "Home"
 	menu_label = "home"
+	content_file = "landing.html"
+	content_is_markdown = False
 
 class Assets(CommonAssets):
 	assets = [('css/normalize.css', 'css/'),
 				('css/main.css', 'css/'),
 				('fonts/', 'fonts/'),
-				('js/jquery-2.2.3.min.js', 'js/'),]
+				('js/jquery-2.2.3.min.js', 'js/'),
+				('images/', 'images/')]
 
 class SeidenstrasseSite(Verdandi):
-	modules = [MainPage(),
+	modules = [LandingPage(),
 				Assets()]
 
 
